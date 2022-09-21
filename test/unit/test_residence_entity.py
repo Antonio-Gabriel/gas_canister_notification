7a69743021422efd879f9eca1850e1a327c15a6d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-21 22:59:00 +0100

    test: test residence entity

---
+++ test/unit/test_residence_entity.py
@@ -0,0 +1,13 @@
+from src.domain.entities.residence import Residence, ResidenceProps
+
+def test_residence_entity():
+    """shold be return a residence id"""
+
+    residence = Residence(1, ResidenceProps(
+        owner='António Gabriel',
+        contacts=['+244987234554', '+244912434232'],
+        building=46,
+        block='24'
+    ))
+    
+    assert residence.get_id is 1
