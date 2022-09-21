7faee61b092cb45578ba08ac7c586a2ffaf90857
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-21 23:47:46 +0100

    feat: create notication repository interface

---
+++ src/domain/repositories/notifications_repository.py
@@ -0,0 +1,20 @@
+from typing import Type
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+@dataclass
+class ResidenceProps:
+    """props of residence entity"""    
+    owner: str
+    contacts: list  
+    building: int
+    block: str
+
+class NotificationsRepository(ABC):
+    """repository for persist historic of notifications"""
+
+    @abstractmethod
+    async def register_notification(self, residence_props: Type[ResidenceProps]):
+        """persist notifications history into storage"""
+
+        raise NotImplementedError('Method not implemented')
