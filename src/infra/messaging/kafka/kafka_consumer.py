49ceecbbde372f9220902a6b4a18d0d2378b4f69
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-23 10:58:44 +0100

    feat: add new props on kafka instance

--- src/infra/messaging/kafka/kafka_consumer.py
+++ src/infra/messaging/kafka/kafka_consumer.py
@@ -1,3 +1,4 @@
+from json import loads
 from kafka import KafkaConsumer as KafkaConsumerInstance
 
 class KafkaConsumer:
@@ -6,11 +7,15 @@ class KafkaConsumer:
     def __init__(self, topic: str) -> None:
         self.__consumer = KafkaConsumerInstance(
             topic,
-            bootstrap_servers='localhost:9092'            
-        )
+            bootstrap_servers='localhost:9092',
+            auto_offset_reset='latest',    
+            enable_auto_commit=True,
+            group_id='gas_canister_notify_group',
+            value_deserializer=lambda x: loads(x.decode('utf-8'))       
+            )
     
     @property
     def get_consumer_data(self):
-        """return consumer data"""
+        """return consumer data"""        
 
         return self.__consumer
