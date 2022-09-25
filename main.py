fde209b28cde41ccee81e70101e39516c3c84458
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-25 18:49:04 +0100

    feat: configure socket io

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from src.infra.messaging.kafka.kafka_producer import KafkaProducer
+from uuid import uuid4
+from flask.app import Flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
 
-# gas_canister.new_notification
+app = Flask(__name__)
+io = SocketIO(app, cors_allowed_origins='*')
 
-producer = KafkaProducer()
-producer.send('gas_canister.new_notification', {
-    'name': "Ag"
-})
+@app.route("/")
+def init():
+    """default route"""
+
+@io.on('connected')
+def connection(msg):
+    """show if exists a new connection into socket"""
+
+    print(msg)
+
+@io.on('notification')
+def handle_notification(data):
+    """watch the broadcast that send a notification"""
+
+    print('received notification: ' + data)
+
+if __name__ == '__main__':
+    io.run(app, debug=True)
