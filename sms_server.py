0a79c4b9b58570b8d8ce9749787e4d188be16e26
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-25 17:05:55 +0100

    add comments describer the next implementation

--- sms_server.py
+++ sms_server.py
@@ -5,5 +5,10 @@ _instance = KafkaConsumer()
 consumer = _instance.get_consumer_data
 consumer.subscribe(['gas_canister.new_notification'])
 
+"""
+    Here i will configure the rules for send sms to users inform that your gas just finish or
+    that your gas have  little time to finish, you need to carrie her
+"""
+
 for msg in consumer:
     print(msg.value)
