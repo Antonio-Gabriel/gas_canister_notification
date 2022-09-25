3aaeac8dd697cf57cf89553fc6da71044520a0c2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-25 13:56:15 +0100

    test: handler communicator test

--- test/integration/test_notification_communicator_handler.py
+++ test/integration/test_notification_communicator_handler.py
@@ -15,14 +15,7 @@ def test_notification_communicator_handler():
 
     """The test for pub and sub events on queue it work, i commented because i don't want 
         that add always message on kafka, for test remove the comments and run the test"""
-    
-    # notification_event = NotificationSubmittedEvent(1, ResidenceProps(
-    #     owner='António Gabriel',
-    #     contacts=['+244987234554', '+244912434232'],
-    #     building=46,
-    #     block='24'
-    # ))
-    
+  
     # notification_usecase = NotificationSubmittUseCase(mediator=mediator)
 
     # residence = Residence(1, ResidenceProps(
