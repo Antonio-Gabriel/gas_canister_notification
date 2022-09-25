afb9c6eb62e578463f0db88d4b3daacb1e171c04
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-25 16:03:42 +0100

    remove consumer

--- consumer.py
+++
@@ -1,9 +0,0 @@
-from src.infra.messaging.kafka.kafka_consumer import KafkaConsumer
-
-_instance = KafkaConsumer()
-
-consumer = _instance.get_consumer_data
-consumer.subscribe(['gas_canister.new_notification'])
-
-for msg in consumer:
-    print(msg.value)
