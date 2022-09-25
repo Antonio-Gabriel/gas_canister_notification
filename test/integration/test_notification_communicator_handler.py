f357e60ef9f224235e78e361a8ff0ad6388efc07
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-09-25 16:02:17 +0100

    refactor: add error handling

--- test/integration/test_notification_communicator_handler.py
+++ test/integration/test_notification_communicator_handler.py
@@ -16,15 +16,18 @@ def test_notification_communicator_handler():
     """The test for pub and sub events on queue it work, i commented because i don't want 
         that add always message on kafka, for test remove the comments and run the test"""
   
-    # notification_usecase = NotificationSubmittUseCase(mediator=mediator)
+    notification_usecase = NotificationSubmittUseCase(mediator=mediator)
 
-    # residence = Residence(1, ResidenceProps(
-    #     owner='António Gabriel',
-    #     contacts=['+244987234554', '+244912434232'],
-    #     building=46,
-    #     block='24'
-    # ))
+    residence = Residence(1, ResidenceProps(
+        owner='Kiala Gabriel',
+        contacts=['+244987234554', '+244912434232'],
+        building=46,
+        block='24'
+    ))
 
-    # asyncio.run(notification_usecase.execute(residence=residence))
+    try:
+        asyncio.run(notification_usecase.execute(residence=residence))
+    except Exception as error:
+        print("An error occurred!", error)
 
     assert 1 == 1
